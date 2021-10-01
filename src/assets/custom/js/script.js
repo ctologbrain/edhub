0ea4800a94a0466e904eb42edca334470fdb8b03
Deepesh Gupta <deepesh.gupta@univoxx.com>
2021-10-01 08:36:36 +0530

    add design changes

--- src/assets/custom/js/script.js
+++ src/assets/custom/js/script.js
@@ -30,12 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-$(".main-menu-dd-item").on("mouseover", function () {
-  let mainDdHeight = $(".cousre-dd-main-menu").outerHeight();
-  $(".cousre-dd-sub-menu").css("min-height", mainDdHeight);
-});
-
-$(".sub-menu-dd-item").on("mouseover", function () {
-  let subDdHeight = $(".cousre-dd-sub-menu").outerHeight();
-  $(".cousre-dd-sub-child-menu").css("min-height", subDdHeight);
+$("#pricingRange").on("input", function () {
+  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
+  this.style.background =
+    "linear-gradient(to right, #f05454 0%, #f05454 " +
+    value +
+    "%, #b5b5b5 " +
+    value +
+    "%, #b5b5b5 100%)";
 });
