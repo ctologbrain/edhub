e42bd7e431eb044d0a294fe327007da27af514c4
Deepesh Gupta <deepesh.gupta@univoxx.com>
2021-10-01 23:25:04 +0530

    add pages

--- src/app/_views/pages/pages.module.ts
+++ src/app/_views/pages/pages.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './Account/profile/profile.component';
+import { CompareComponent } from './compare/compare.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'compare',
+    component: CompareComponent,
+  },
 ];
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ProfileComponent, CompareComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class PagesModule {}
