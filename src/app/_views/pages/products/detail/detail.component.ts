079477a50655f7704145a53b0017842ec08d0830
Deepesh Gupta <deepesh.gupta@univoxx.com>
2021-10-06 10:09:53 +0530

    add detail

--- src/app/_views/pages/products/detail/detail.component.ts
+++ src/app/_views/pages/products/detail/detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/_core/services/product.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'product-detail',
@@ -7,7 +9,31 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.component.scss'],
 })
 export class DetailComponent implements OnInit {
-  constructor() {}
+  courseDetails: any;
+  serverUrl = environment.server_url;
+  constructor(
+    private _product: ProductService,
+    private _activated: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  async ngOnInit() {
+    this._activated.paramMap.subscribe(async (param) => {
+      let pdoduct_slug = param.get('coursename');
+      let res = await this._product.getCourseDetails({ pdoduct_slug });
+      if (res.status == 'true') {
+        this.courseDetails = res.data;
+        this.courseDetails.faq = this.courseDetails.faq
+          ? JSON.parse(this.courseDetails.faq)
+          : [];
+        this.courseDetails.prerequisites = this.courseDetails.prerequisites
+          ? JSON.parse(this.courseDetails.prerequisites)
+          : [];
+        this.courseDetails.outcome = this.courseDetails.outcome
+          ? JSON.parse(this.courseDetails.outcome)
+          : [];
+        // console.log(this.courseDetails);
+      } else {
+      }
+    });
+  }
 }
