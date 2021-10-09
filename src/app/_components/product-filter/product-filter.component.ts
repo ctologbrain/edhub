770cc4bd1a9e23930d61b7e81ec0934684b3da69
Deepesh Gupta <deepesh.gupta@univoxx.com>
2021-10-09 17:05:56 +0530

    add filter

--- src/app/_components/product-filter/product-filter.component.ts
+++ src/app/_components/product-filter/product-filter.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonService } from 'src/app/_core/services/common.service';
+import { environment } from 'src/environments/environment';
 declare let $: any;
 @Component({
   selector: 'app-product-filter',
@@ -8,11 +9,22 @@ declare let $: any;
 })
 export class ProductFilterComponent implements OnInit {
   @Input() mobile = false;
+  @Output() applyFilter = new EventEmitter();
   minPrice = 0;
   maxPrice = 150000;
-  filterData = {
+  serverurl = environment.server_url;
+  filterData: { languages: any; providers: any } = {
     languages: null,
     providers: null,
+  };
+
+  filters: any = {
+    price_start: this.minPrice,
+    prince_end: this.maxPrice,
+    course_provider: [],
+    rating: [],
+    language: [],
+    certification: [],
   };
   constructor(private _common: CommonService) {}
 
@@ -34,7 +46,7 @@ export class ProductFilterComponent implements OnInit {
 
   initSlider() {
     const current = this;
-    $('.slider-range').slider({
+    const slider = {
       range: true,
       min: 0,
       max: 150000,
@@ -47,10 +59,39 @@ export class ProductFilterComponent implements OnInit {
         $(this).slider('values', 0, current.minPrice);
         $(this).slider('values', 1, current.maxPrice);
       },
-    });
+      stop: function () {
+        current.minPrice = $(this).slider('values', 0);
+        current.maxPrice = $(this).slider('values', 1);
+        current.changeFilter();
+      },
+    };
+    $('.slider-range').slider(slider);
     $('.min-max-input').change(function (e: any) {
       var setIndex = e.currentTarget.id == 'max-price-input' ? 1 : 0;
       $('.slider-range').slider('values', setIndex, $(e.currentTarget).val());
     });
+  }
+
+  setFilter(e: any, type: string) {
+    let index = this.filters[type].indexOf(e.target.value.trim());
+    if (index == -1) {
+      this.filters[type].push(e.target.value.trim());
+    } else {
+      this.filters[type].splice(index, 1);
+    }
+    this.changeFilter();
+  }
+
+  changeFilter() {
+    let filter: any = {};
+    this.filters.price_start = this.minPrice;
+    this.filters.prince_end = this.maxPrice;
+    for (const key in this.filters) {
+      if (Object.prototype.hasOwnProperty.call(this.filters, key)) {
+        const element = this.filters[key];
+        filter[key] = element.toString();
+      }
+    }
+    this.applyFilter.next(filter);
   }
 }
--- src/app/_views/pages/products/products.component.ts
+++ src/app/_views/pages/products/products.component.ts
@@ -15,7 +15,7 @@ export class ProductsComponent implements OnInit {
     course_topic: null,
     start: 1,
     price_start: 0,
-    prince_end: 1000,
+    prince_end: 150000,
     course_provider: '',
     rating: '',
     language: '',
@@ -23,7 +23,7 @@ export class ProductsComponent implements OnInit {
   };
   count = 0;
   loading = new BehaviorSubject(false);
-  products: any[] = [];
+  products: any = null;
   constructor(
     private _activated: ActivatedRoute,
     private _product: ProductService
@@ -47,5 +47,30 @@ export class ProductsComponent implements OnInit {
     let res = await this._product.getCourses(this.params);
     this.products.push(...res.data.rows);
     this.loading.next(false);
+  }
+
+  async applyFilter(filter: any) {
+    this.products = null;
+    this.count = 0;
+    this.params.price_start = filter.price_start;
+    this.params.prince_end = filter.prince_end;
+    this.params.course_provider = filter.course_provider;
+    this.params.rating = filter.rating;
+    this.params.language = filter.language;
+    this.params.certification = filter.certification;
+    this.params.start = 1;
+    let res = await this._product.getCourses(this.params);
+    this.products = res.data.rows;
+    this.count = res.data.count;
+  }
+
+  resetFilter() {
+    this.params.price_start = 0;
+    this.params.prince_end = 150000;
+    this.params.course_provider = '';
+    this.params.rating = '';
+    this.params.language = '';
+    this.params.certification = '';
+    this.applyFilter(this.params);
   }
 }
